1407ddb594f73b91c21f05eecf367e1ec4156de4
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-03-07 01:49:55 +0200

    sending emails

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
+from django.core.mail import send_mail
+from loginsystem.settings import EMAIL_HOST_USER
 # from django.http import HttpResponse
 # Create your views here.
 def home(request):
@@ -15,7 +16,26 @@ def signup(request):
         lname = request.POST['lname']
         email = request.POST['email']
         pass1 = request.POST['pass1']
-        # pass2 = request.POST['pass2']
+        pass2 = request.POST['pass2']
+        
+        # Tests and validations
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists")
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered")
+            return redirect('home')
+        
+        if len(username)>10:
+            messages.error(request,"Username must have less that 10 letters")
+            
+        if pass1 != pass2:
+            messages.error(request,"Passwords does not match")
+            
+        if not username.isalnum():
+            messages.error(request, 'Your username must contain letters and numbers only')
+            return redirect('home')
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
@@ -23,8 +43,15 @@ def signup(request):
         
         myuser.save()
         
-        messages.success(request, 'Account successfully created'
-                         )
+        messages.success(request, 'Account successfully created')
+        
+        # sending emails
+        subject = 'Welcome to login pages'
+        message = "Hello " + myuser.first_name +" || \n Welcome to our website! \n We have lso sent you are confirmation code. Please check your email and confirm your account in order to activate your account! \n \n THANK YOU"
+        from_email = EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list)
+        
         return redirect('signin')
         
     return render(request,'authentication/signup.html')
